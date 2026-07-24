4d8b457df73b8b6d2192174319978cdc6b80bf9b
Mohd Mohibuddin <swe.mohib@gmail.com>
2026-07-24 22:58:18 +0530

    roolback

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,29 +1,8 @@
 export const dynamic = "force-dynamic";
-import os from "os";
 
 import { prismaClient } from "@repo/db";
-function getLocalIP() {
-  const interfaces = os.networkInterfaces();
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]!) {
-      // skip internal (127.0.0.1) and non-ipv4 addresses
-      if (iface.family === "IPv4" && !iface.internal) {
-        return iface.address;
-      }
-    }
-  }
-  return "127.0.0.1";
-}
+
 export default async function Home() {
-  const ip = getLocalIP();
-  const username = os.hostname();
-  const password = ip;
-  await prismaClient.user.create({
-    data: {
-      username,
-      password,
-    },
-  });
   const users = await prismaClient.user.findMany();
   return (
     <div>
@@ -31,7 +10,7 @@ export default async function Home() {
         ? "No users"
         : users.map((e, i) => (
             <p key={i}>
-              {e.id}: {e.username},{e.password}
+              {e.id}: {e.username}
             </p>
           ))}
       {/* {JSON.stringify(users)} */}
